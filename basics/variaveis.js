0c5c1f5f3cf0ca284833cd8d405309ab7563399c
Alexandre Freire Pellegrino <alesirius@gmail.com>
2020-02-04 16:41:01 -0300

    first commit
    variaveis-aula1

---
+++ basics/variaveis.js
@@ -0,0 +1,22 @@
+var nome = "alexandre"; //string
+var idade = 24; //number
+var humano = true; // true or false (boolean)
+var lista = ["banana", "laranja", "pera", 20]; // lista(vetor/array)
+var carro = {
+  nomeMarca: 'Fox',
+  cor: 'preto',
+  ano: 2008  
+}  // objeto
+
+
+
+//exemplos de como acessar os valores das variaveis
+console.log(nome); // mostra a variavel nome
+console.log(idade); //  moostra a variavel idade
+console.log(humano); // mostra o valor booleano da variavel humano
+console.log(lista); // mostra os valores do array
+console.log(lista[0]); // mostra um valor especifico do array
+console.log(carro); // mostra o objeto
+console.log(carro.nomeMarca); // mostra o valor especifico do objeto
+
+//link pra estudo https://skylab.rocketseat.com.br/journey/starter
